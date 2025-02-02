45c28169c4c4a261bbc8548f39bcc3ade6c861b6
Zahar Dudar <zahar5070@gmail.com>
2025-02-02 11:21:46 +0300

    Done Thomas algorithm

--- src/LinearSystemSolver.h
+++ src/LinearSystemSolver.h
@@ -19,9 +19,11 @@ public:
     T operator()(int i, int j, int set_val){ return matrix[i * nx + j]=set_val;}
     ~Matrix(){};
     static Matrix<T> create_diag_matrix(std::vector<T>);
+    static Matrix<T> create_matrix_from_array(std::vector<T>, int, int);
     static Matrix<T> create_3diag_matrix(std::vector<T>,std::vector<T>,std::vector<T>);
 };
 
+//Create diagonal matrix from vector
 template<typename T>
 Matrix<T> Matrix<T>::create_diag_matrix(std::vector<T> diag){
     Matrix<T> tmp_matrix;
@@ -44,12 +46,60 @@ Matrix<T> Matrix<T>::create_3diag_matrix(std::vector<T> a , std::vector<T> b, st
 
     Matrix<T> tmp_matrix = Matrix<T>::create_diag_matrix(b);
     for(int i=0; i<tmp_matrix.nx-1; i++){
-        tmp_matrix(i,i+1,a[i]);
+        tmp_matrix(i+1,i,a[i]);
     }
     for(int i=0; i<tmp_matrix.nx-1; i++){
-        tmp_matrix(i+1, i, c[i]);
+        tmp_matrix(i, i+1, c[i]);
     }
     return tmp_matrix;
+}
+
+// Create matrix from given array with given shape
+template<typename T>
+Matrix<T> Matrix<T>::create_matrix_from_array(std::vector<T> a, int nx, int ny){
+    if(a.size() != (nx * ny)) {std::cout<<"Bad shape!"; throw 1;}
+
+    Matrix<T> tmp_matrix;
+    for(int i=0; i<nx*ny; i++){
+        tmp_matrix.matrix = a;
+    }
+    tmp_matrix.nx = nx;
+    tmp_matrix.ny = ny;
+    return tmp_matrix;
+}
+
+
+
+
+// _________
+template<typename T>
+std::vector<T> SolveThomasAglorithm(Matrix<T> A, std::vector<T> B){
+    std::vector<T> p;
+    std::vector<T> q;
+    std::vector<T> x;
+    if(A.shape().first != A.shape().second){std::cout<<"Matrix not square!"; throw 1;}
+    int n = A.shape().first;
+
+    p.resize(n);
+    q.resize(n);
+    x.resize(n);
+
+    //Прямой ход
+    p[0] = - A(0,1) / A(0,0);
+    q[0] = B[0] / A(0,0);
+
+    for(int i=1; i<n-1; i++){
+        p[i] = - A(i, i+1) / (A(i,i-1) * p[i-1] + A(i,i));
+        q[i] = (B[i] - A(i,i-1) * q[i-1]) / (A(i,i-1) * p[i-1] + A(i, i));
+    }
+
+    //Обратный ход
+    x[n-1] = (B[n-1] - A(n-1,n-2) * q[n-2]) / (A(n-1,n-2) * p[n-2] + A(n-1, n-1));
+    for(int i=n-2; i>=0; i--){
+        x[i] = x[i+1] * p[i] + q[i];
+    }
+
+    return x;
 }
 
 #endif 
--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <LinearSystemSolver.h>
-// Demonstrate some basic assertions.
+//Check creating diagonal matrix from vector
 TEST(MatrixClass, DiagGen) {
 	std::vector<int> diag{1,2,3};
 	Matrix<int> test_mtx = Matrix<int>::create_diag_matrix(diag);
@@ -20,19 +20,60 @@ TEST(MatrixClass, DiagGen) {
 	}
 }
 
+//Check creating array with main and 2 nearest diags
 TEST(MatrixClass, ThreeDiagGen) {
 	std::vector<int> diagA{1,1};
 	std::vector<int> diagB{2,2,2};
 	std::vector<int> diagC{3,3};
-	EXPECT_ANY_THROW(Matrix<int>::create_3diag_matrix(diagA, diagC, diagA)) << "Does'n throw";
+	EXPECT_ANY_THROW(Matrix<int>::create_3diag_matrix(diagA, diagC, diagA)) << "Doesn't throw";
 	Matrix<int> test_mtx = Matrix<int>::create_3diag_matrix(diagA, diagB, diagC);
 	EXPECT_EQ(test_mtx.shape(), std::make_pair(3,3)) << "Wrong shape!";
 
     for(int i=0; i<2; i++){
-        EXPECT_EQ(test_mtx(i,i+1), diagA[i]) << "Wrong diagA";
+        EXPECT_EQ(test_mtx(i+1,i), diagA[i]) << "Wrong diagA";
     }
     for(int i=0; i<2; i++){
-		EXPECT_EQ(test_mtx(i+1,i), diagC[i]) << "Wrong diagC";
+		EXPECT_EQ(test_mtx(i,i+1), diagC[i]) << "Wrong diagC";
     }
+}
+
+//Check for creating matrix from one-dim array with given shape
+TEST(MatrixClass, MatrixFromArray) {
+	std::vector<int> a{1,2,3,
+					   4,5,6,
+					   7,8,9};
+	EXPECT_ANY_THROW(Matrix<int>::create_matrix_from_array(a, 2, 3)) << "Doesn't throw";
+	Matrix<int> test_mtx = Matrix<int>::create_matrix_from_array(a, 3, 3);
+	EXPECT_EQ(test_mtx.shape(), std::make_pair(3,3)) << "Wrong shape!";
+
+	for(int i=0; i<3; i++){
+		for(int j=0; j<3 ; j++){
+			EXPECT_EQ(test_mtx(i,j), a[i*3+j]);
+		}
+	}
+	EXPECT_EQ(test_mtx(1,0), 4);
+}
+
+
+TEST(ThomasAlgorithm, Correct) {
+	std::vector<float> a{1,2,0,
+					     4,5,6,
+					     0,8,9};
+	Matrix<float> A = Matrix<float>::create_matrix_from_array(a, 3, 3);
+	std::vector<float> B{8,47,60};
+
+	std::vector<float> right_solution{2, 3, 4};
+	std::vector<float> getted_solution = SolveThomasAglorithm(A,B);
+	for(int i=0; i<3; i++){
+		EXPECT_NEAR(getted_solution[i],right_solution[i], 0.0001);
+	}
+	
+	B =std::vector<float>{1,2,3};
+
+	right_solution = std::vector<float>{0.04, 0.48, -0.0933};
+    getted_solution = SolveThomasAglorithm(A,B);
+	for(int i=0; i<3; i++){
+		EXPECT_NEAR(getted_solution[i],right_solution[i], 0.0001);
+	}
 }
 
