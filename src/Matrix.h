f59be581fa7fe867b756cd8613451d17498769ca
Zahar Dudar <zahar5070@gmail.com>
2025-02-06 00:28:42 +0300

    Added SCR matrix with tools, multiplying by vector, fix trouble with naming 'x', 'y', 'row', 'col'

---
+++ src/Matrix.h
@@ -0,0 +1,67 @@
+#pragma once
+#include <vector>
+
+
+template<typename T>
+class Matrix
+{
+private:
+    int nc;
+    int nr;
+    std::vector<T> matrix;
+public:
+    Matrix(){nc = 0; nr = 0;};
+    std::pair<int, int> shape(){ return std::make_pair(nr, nc);}
+    // T& operator()(int i, int j){ return matrix[i * nc + j];} //Проблема если где-то прописать vector.push_back(mtrx(i,j)), в вектор кажется добавится ссылка на элемент и его можно будет поменять оттуда 
+    T operator()(int i, int j){ return matrix[i * nc + j];} 
+    T operator()(int i, int j, int set_val){ return matrix[i * nc + j]=set_val;}
+    ~Matrix(){};
+    static Matrix<T> create_diag_matrix(std::vector<T>);
+    static Matrix<T> create_matrix_from_array(std::vector<T>, int, int);
+    static Matrix<T> create_3diag_matrix(std::vector<T>,std::vector<T>,std::vector<T>);
+};
+
+//Create diagonal matrix from vector
+template<typename T>
+Matrix<T> Matrix<T>::create_diag_matrix(std::vector<T> diag){
+    Matrix<T> tmp_matrix;
+    tmp_matrix.nc = diag.size();
+    tmp_matrix.nr = diag.size();
+    for(int i=0; i<tmp_matrix.nc * tmp_matrix.nr; i++){
+        tmp_matrix.matrix.push_back(0);
+    }
+    for(int i=0; i<diag.size(); i++){
+        tmp_matrix(i,i,diag[i]);
+    }
+    return tmp_matrix;
+}
+
+
+//Create 3-diagonal matrix with main diagonal b. Essential: lenght b = lenght a + 1 = lenght c + 1 
+template<typename T>
+Matrix<T> Matrix<T>::create_3diag_matrix(std::vector<T> a , std::vector<T> b, std::vector<T> c){
+    if(a.size() != c.size() or a.size() != (b.size()-1)) {std::cout<<"Bad diagonals lenghts!"; throw 1;}
+
+    Matrix<T> tmp_matrix = Matrix<T>::create_diag_matrix(b);
+    for(int i=0; i<tmp_matrix.nc-1; i++){
+        tmp_matrix(i+1,i,a[i]);
+    }
+    for(int i=0; i<tmp_matrix.nc-1; i++){
+        tmp_matrix(i, i+1, c[i]);
+    }
+    return tmp_matrix;
+}
+
+// Create matrix from given array with given shape
+template<typename T>
+Matrix<T> Matrix<T>::create_matrix_from_array(std::vector<T> a, int nr, int nc){
+    if(a.size() != (nc * nr)) {std::cout<<"Bad shape!"; throw 1;}
+
+    Matrix<T> tmp_matrix;
+    for(int i=0; i<nc*nr; i++){
+        tmp_matrix.matrix = a;
+    }
+    tmp_matrix.nc = nc;
+    tmp_matrix.nr = nr;
+    return tmp_matrix;
+}
---
+++ src/SCRMatrix.h
@@ -0,0 +1,140 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <Matrix.h>
+#include <iostream>
+
+
+template<typename T>
+class SCRMatrix
+{
+private:
+    int nc;
+    int nr;
+    int N_non_zero;
+    std::vector<T> val;
+    std::vector<int> cols;
+    std::vector<int> rows;
+    SCRMatrix(){};
+
+public:
+    SCRMatrix(std::map<std::pair<int, int>, T>);
+    std::pair<int, int> shape(){ return std::make_pair(nr, nc);}
+    T operator()(int r, int c){
+        for(int ind=rows[r]; ind<rows[r+1]; ind++){
+            if(cols[ind] == c){
+                return val[ind];
+            }
+        }
+        return (T)0;
+    }
+    std::vector<T> operator*(std::vector<T>B);
+    static SCRMatrix<T> SCR_from_reg_matrix(Matrix<T>);
+    ~SCRMatrix(){};
+};
+
+
+
+
+template<typename T>
+SCRMatrix<T>::SCRMatrix(std::map<std::pair<int, int>, T> in){
+    nc=0;
+    nr=0;
+    N_non_zero=0;
+    rows.push_back(0);
+
+    for (auto const &el : in){
+        if(el.first.first >= nc){
+            nc = el.first.first;
+        }
+        if(el.first.second >= nr){
+            nr = el.first.second;
+        }
+    }
+    //nr and nc after cycle is maximum indxs but it has to be number in a col and in a row relative
+    nr++; 
+    nc++;
+
+    std::vector<T> zero_diag(nc*nr);
+    for(int i=0; i<nc*nr; i++) {zero_diag[i]=0;}
+
+    Matrix<T> tmp_matrix = Matrix<T>::create_matrix_from_array(zero_diag, nc, nr);
+    for (auto const &el : in){
+        tmp_matrix(el.first.first, el.first.second, el.second);
+    }
+    
+    
+    for(int i=0; i<nr; i++){
+        for(int j=0; j<nc; j++){
+            if(tmp_matrix(i,j)!=0){
+                val.push_back(tmp_matrix(i,j));
+                cols.push_back(j);
+                N_non_zero++;
+            }
+        }
+        rows.push_back(N_non_zero);
+    }
+
+}
+
+template<typename T>
+Matrix<T> reg_matrix_from_SCR(SCRMatrix<T> A){
+    int nc = A.shape().first;
+    int nr = A.shape().second;
+
+
+    std::vector<T> zero_diag(nc*nr);
+    for(int i=0; i<nc*nr; i++) {zero_diag[i]=0;}
+
+    Matrix<T> tmp_matrix = Matrix<T>::create_diag_matrix(zero_diag);
+
+    for(int i=0; i<nr; i++){
+        for(int j=0; j<nr; j++){
+            tmp_matrix(i,j,A(i,j));
+        }
+    }
+    return tmp_matrix;
+}
+
+
+template<typename T>
+SCRMatrix<T> SCRMatrix<T>::SCR_from_reg_matrix(Matrix<T> A){
+    int nr = A.shape().first;
+    int nc = A.shape().second;
+
+    SCRMatrix<T> tmp_matrix;
+    tmp_matrix.nc = nc;
+    tmp_matrix.nr = nr;
+    tmp_matrix.N_non_zero = 0;
+    tmp_matrix.rows.push_back(0);
+
+    for(int i=0; i<nr; i++){
+        for(int j=0; j<nc; j++){
+            if(A(i,j)!=0){
+                tmp_matrix.val.push_back(A(i,j));
+                tmp_matrix.cols.push_back(j);
+                tmp_matrix.N_non_zero++;
+            }
+        }
+        tmp_matrix.rows.push_back(tmp_matrix.N_non_zero);
+    }
+    return tmp_matrix;
+}
+
+template<typename T>
+std::vector<T> SCRMatrix<T>::operator*(std::vector<T>B){
+    if(this->shape().second != B.size()) {std::cout<<"lenght row != length B"; throw 1;}
+    std::vector<T> result(this->shape().first);
+
+    T tmp_sum=0;
+    for(int i=0; i<this->shape().first; i++){
+        tmp_sum = 0;
+        for(int j=this->rows[i]; j<this->rows[i+1]; j++){
+            tmp_sum += this->val[j] * B[cols[j]];
+        }
+        result[i] = tmp_sum;
+    }
+    return result;
+}
