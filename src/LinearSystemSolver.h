72dd115548ff331ab796af3ceceff94ee35f3cb1
Zahar Dudar <zahar5070@gmail.com>
2025-02-14 20:08:04 +0300

    fix args in thomas solver

--- src/LinearSystemSolver.h
+++ src/LinearSystemSolver.h
@@ -15,7 +15,7 @@
 
 // _________
 template<typename T>
-std::vector<T> SolveThomasAglorithm(Matrix<T> A, std::vector<T> B){
+std::vector<T> SolveThomasAglorithm(const Matrix<T>& A,const std::vector<T>& B){
     std::vector<T> p;
     std::vector<T> q;
     std::vector<T> x;
@@ -37,6 +37,36 @@ std::vector<T> SolveThomasAglorithm(Matrix<T> A, std::vector<T> B){
 
     //Обратный ход
     x[n-1] = (B[n-1] - A(n-1,n-2) * q[n-2]) / (A(n-1,n-2) * p[n-2] + A(n-1, n-1));
+    for(int i=n-2; i>=0; i--){
+        x[i] = x[i+1] * p[i] + q[i];
+    }
+
+    return x;
+}
+
+template<typename T>
+std::vector<T> SolveThomasAglorithm(const std::vector<T>& a, const std::vector<T>& b, const std::vector<T>& c, const std::vector<T>& B){
+    std::vector<T> p;
+    std::vector<T> q;
+    std::vector<T> x;
+    if((a.size() != c.size()) and b.size()-1 != a.size()){std::cout<<"Wrong lenghts!"; throw 1;}
+    int n = b.size();
+
+    p.resize(n);
+    q.resize(n);
+    x.resize(n);
+
+    //Прямой ход
+    p[0] = - c[0] / b[0];
+    q[0] = B[0] / b[0];
+
+    for(int i=1; i<n-1; i++){
+        p[i] = - c[i] / (a[i-1] * p[i-1] + b[i]);
+        q[i] = (B[i] - a[i-1] * q[i-1]) / (a[i-1] * p[i-1] + b[i]);
+    }
+
+    //Обратный ход
+    x[n-1] = (B[n-1] - a[n-2] * q[n-2]) / (a[n-2] * p[n-2] + b[n-1]);
     for(int i=n-2; i>=0; i--){
         x[i] = x[i+1] * p[i] + q[i];
     }
--- tests/TestSolving.cpp
+++ tests/TestSolving.cpp
@@ -6,20 +6,28 @@ TEST(ThomasAlgorithm, Correct) {
 	std::vector<float> a{1,2,0,
 					     4,5,6,
 					     0,8,9};
+
+	std::vector<float> a_line{4,8};
+	std::vector<float> b_line{1,5,9};
+	std::vector<float> c_line{2,6};
 	Matrix<float> A = Matrix<float>::create_matrix_from_array(a, 3, 3);
 	std::vector<float> B{8,47,60};
 
 	std::vector<float> right_solution{2, 3, 4};
-	std::vector<float> getted_solution = SolveThomasAglorithm(A,B);
+	std::vector<float> getted_solution_dense = SolveThomasAglorithm(A,B);
+	std::vector<float> getted_solution_threeDiag = SolveThomasAglorithm(a_line, b_line, c_line ,B);
 	for(int i=0; i<3; i++){
-		EXPECT_NEAR(getted_solution[i],right_solution[i], 0.0001);
+		EXPECT_NEAR(getted_solution_dense[i],right_solution[i], 0.0001);
+		EXPECT_NEAR(getted_solution_threeDiag[i],right_solution[i], 0.0001);
 	}
 	
 	B =std::vector<float>{1,2,3};
 
 	right_solution = std::vector<float>{0.04, 0.48, -0.0933};
-    getted_solution = SolveThomasAglorithm(A,B);
+    getted_solution_dense = SolveThomasAglorithm(A,B);
+    getted_solution_threeDiag = SolveThomasAglorithm(a_line, b_line, c_line ,B);
 	for(int i=0; i<3; i++){
-		EXPECT_NEAR(getted_solution[i],right_solution[i], 0.0001);
+		EXPECT_NEAR(getted_solution_dense[i],right_solution[i], 0.0001);
+		EXPECT_NEAR(getted_solution_threeDiag[i],right_solution[i], 0.0001);
 	}
 }
