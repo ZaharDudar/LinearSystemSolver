894645febfd9c1c2f0d176a22b4a24c59d3b5451
Zahar Dudar <zahar5070@gmail.com>
2025-04-06 16:19:20 +0300

    improve SORvsOmega

--- timeMesure/plotters/plotSORvsOmega.py
+++ timeMesure/plotters/plotSORvsOmega.py
@@ -15,9 +15,12 @@ data = np.array(data)
 fig, axs = plt.subplots(1, 2)
 print(names)
 for i in range(1,len(names)-1,2):
-    axs[0].plot(data[np.where(data[:,i]>1e-7)][:,0], data[np.where(data[:,i]>1e-7)][:,i],"--", label=" ".join(names[i].split("_")[:-1]))
-    axs[1].plot(data[np.where(data[:,i]>1e-7)][:,i+1], data[np.where(data[:,i]>1e-7)][:,i],"--", label=" ".join(names[i].split("_")[:-1]))
-    
+    if(" ".join(names[i].split("_")[:-1]) == "SOR optimal"):
+        axs[0].plot(data[np.where(data[:,i]>1e-7)][:,0], data[np.where(data[:,i]>1e-7)][:,i],"--", label=" ".join(names[i].split("_")[:-1]), color='black')
+        axs[1].plot(data[np.where(data[:,i]>1e-7)][:,i+1], data[np.where(data[:,i]>1e-7)][:,i],"--", label=" ".join(names[i].split("_")[:-1]), color='black')
+    else:
+        axs[0].plot(data[np.where(data[:,i]>1e-7)][:,0], data[np.where(data[:,i]>1e-7)][:,i],"--", label=" ".join(names[i].split("_")[:-1]))
+        axs[1].plot(data[np.where(data[:,i]>1e-7)][:,i+1], data[np.where(data[:,i]>1e-7)][:,i],"--", label=" ".join(names[i].split("_")[:-1]))
 axs[0].set_yscale("log")
 axs[1].set_yscale("log")
 axs[0].grid()
